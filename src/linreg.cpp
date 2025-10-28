727136f2cdbd7737cc5723f475c33cdbc42c512f
Pratham Nayak <protomn@users.noreply.github.com>
2025-10-28 18:35:53 +1100

    Defined and implemented the fit() algorithm.

--- src/linreg.cpp
+++ src/linreg.cpp
@@ -49,7 +49,76 @@ LinearRegression::LinearRegression(const std::string &method,
 void LinearRegression::fit(const Eigen::Ref<const Eigen::MatrixXd> &X,
                            const Eigen::Ref<const Eigen::VectorXd> &y)
 {
+    if (X.rows() == 0 || X.cols() == 0)
+    {
+        throw std::invalid_argument("Feature matrix cannot be empty.");
+    }
 
+    if (X.rows() != y.size())
+    {
+        throw std::invalid_argument("Number of samples in X should match size of y.");
+    }
+
+    Eigen::MatrixXd X_aug;
+
+    if (fit_intercept)
+    {
+        X_aug.resize(X.rows(), X.cols() + 1);
+        X_aug.leftCols(X.cols()) = X;
+        X_aug.rightCols(1).setOnes();
+    }
+    else
+    {
+        X_aug = X; //shallow copy.
+    }
+
+    Eigen::MatrixXd XtX = X_aug.transpose() * X_aug;
+    Eigen::VectorXd Xty = X_aug.transpose() * y;
+
+    //Ridge Regularization
+
+    if (l2 > 0.0)
+    {
+        Eigen::MatrixXd I = Eigen::MatrixXd::Identity(XtX.rows(), XtX.cols());
+
+        //Skip last element if fit_intercept = true
+
+        if (fit_intercept)
+        {
+            I(I.rows() - 1, I.cols() - 1) = 0.0;
+        }
+        
+        XtX += l2 * I;
+    }
+
+    Eigen::LDLT<Eigen::MatrixXd> solver(XtX);
+    Eigen::VectorXd w_solved = solver.solve(Xty);
+
+    if (solver.info() != Eigen::Success)
+    {
+        throw std::runtime_error("LDLT decomposition failed: possibly singular matrix.");
+    }
+
+    if(!w_solved.allFinite())
+    {
+        throw std::runtime_error("Weights contain NaN/Inf values.");
+    }
+
+    //Separating the feature weights and bias term.
+
+    if (fit_intercept)
+    {
+        w = w_solved.head(w_solved.size() - 1);
+        b = w_solved[w_solved.size() - 1];
+    }
+    else
+    {
+        w = w_solved;
+        b = 0.0;
+    }
+
+    fit_flag = true;
+    std::cout << "Training complete. Coefficients: " << w.transpose() << ", bias: " << b << std::endl;
 }
 
 // Defining the predict method algorithm.
