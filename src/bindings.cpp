988b823ca2f387ccfe8591ea43d6eaa678fc93fd
Pratham Nayak <protomn@users.noreply.github.com>
2025-10-29 03:52:17 +1100

    Implemented bindings.cpp

---
+++ src/bindings.cpp
@@ -0,0 +1,41 @@
+#include "pybind11/pybind11.h"
+#include "pybind11/eigen.h"
+#include "pybind11/numpy.h"
+#include "linreg.hpp"
+#include <string>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_core, m)
+{
+    m.doc() = "Hybrid Linear Regression Module using C++ and pybind11";
+
+    py::class_<LinearRegression> cls(m, "LinearRegression");
+
+    cls.def(py::init<const std::string &, double, double, int, bool>(),
+            py::arg("method"), py::arg("l2") = 0.0, py::arg("learning_rate") = 0.01,
+            py::arg("epochs") = 1000, py::arg("fit_intercept") = true);
+
+    cls.def("fit",
+            [](LinearRegression &self,
+               const Eigen::Ref<const Eigen::MatrixXd> &X,
+               const Eigen::Ref<const Eigen::VectorXd> &y) -> LinearRegression &
+               {
+                py::gil_scoped_release release; //Python GIL released
+                self.fit(X, y); //C++ Computation happens here
+                return self; //Return self for method chaining
+               },
+            py::arg("X"),
+            py::arg("y"));
+
+    cls.def("predict",
+            &LinearRegression::predict,
+            py::arg("X"));
+    
+    cls.def("coef_",
+            &LinearRegression::coef_);
+}
+
+
+
+
