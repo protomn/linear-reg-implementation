2d3ec2934a177acd0573da2834f2a02b18617da5
Pratham Nayak <protomn@users.noreply.github.com>
2025-10-29 14:39:07 +1100

    Implementation of Gradient Descent Algorithm in fit() module.

--- src/linreg.cpp
+++ src/linreg.cpp
@@ -36,6 +36,11 @@ LinearRegression::LinearRegression(const std::string &method,
         throw std::invalid_argument("Learning rate must always be positive.");
     }
 
+    if (learning_rate > 1.0)
+    {
+        std::cerr << "Warning: learning_rate is unusually high (" << learning_rate << ")\n";
+    }
+
     if (method != "normal_equation" && method != "gradient_descent")
     {
         throw std::invalid_argument("Method must be either 'normal_equation' or 'gradient_descent'.");
@@ -67,66 +72,122 @@ void LinearRegression::fit(const Eigen::Ref<const Eigen::MatrixXd> &X,
         throw std::invalid_argument("Number of samples in X should match size of y.");
     }
 
-    Eigen::MatrixXd X_aug;
-
-    if (fit_intercept)
+    if (method == "normal_equation")
     {
-        X_aug.resize(X.rows(), X.cols() + 1);
-        X_aug.leftCols(X.cols()) = X;
-        X_aug.rightCols(1).setOnes();
-    }
-    else
-    {
-        X_aug = X; //shallow copy.
-    }
-
-    Eigen::MatrixXd XtX = X_aug.transpose() * X_aug;
-    Eigen::VectorXd Xty = X_aug.transpose() * y;
-
-    //Ridge Regularization
-
-    if (l2 > 0.0)
-    {
-        Eigen::MatrixXd I = Eigen::MatrixXd::Identity(XtX.rows(), XtX.cols());
-
-        //Skip last element if fit_intercept = true
+        Eigen::MatrixXd X_aug;
 
         if (fit_intercept)
         {
-            I(I.rows() - 1, I.cols() - 1) = 0.0;
+            X_aug.resize(X.rows(), X.cols() + 1);
+            X_aug.leftCols(X.cols()) = X;
+            X_aug.rightCols(1).setOnes();
         }
+        else
+        {
+            X_aug = X; //shallow copy.
+        }
+
+        Eigen::MatrixXd XtX = X_aug.transpose() * X_aug;
+        Eigen::VectorXd Xty = X_aug.transpose() * y;
+
+        //Ridge Regularization
+
+        if (l2 > 0.0)
+        {
+            Eigen::MatrixXd I = Eigen::MatrixXd::Identity(XtX.rows(), XtX.cols());
+
+            //Skip last element if fit_intercept = true
+
+            if (fit_intercept)
+            {
+                I(I.rows() - 1, I.cols() - 1) = 0.0;
+            }
         
-        XtX += l2 * I;
-    }
+            XtX += l2 * I;
+        }
 
-    Eigen::LDLT<Eigen::MatrixXd> solver(XtX);
-    Eigen::VectorXd w_solved = solver.solve(Xty);
+        Eigen::LDLT<Eigen::MatrixXd> solver(XtX);
+        Eigen::VectorXd w_solved = solver.solve(Xty);
 
-    if (solver.info() != Eigen::Success)
-    {
-        throw std::runtime_error("LDLT decomposition failed: possibly singular matrix.");
-    }
+        if (solver.info() != Eigen::Success)
+        {   
+            throw std::runtime_error("LDLT decomposition failed: possibly singular matrix.");
+        }
 
-    if(!w_solved.allFinite())
-    {
-        throw std::runtime_error("Weights contain NaN/Inf values.");
-    }
+        if(!w_solved.allFinite())
+        {
+            throw std::runtime_error("Weights contain NaN/Inf values.");
+        }
 
-    //Separating the feature weights and bias term.
+        //Separating the feature weights and bias term.
 
-    if (fit_intercept)
-    {
-        w = w_solved.head(w_solved.size() - 1);
-        b = w_solved[w_solved.size() - 1];
-    }
-    else
-    {
-        w = w_solved;
-        b = 0.0;
-    }
+        if (fit_intercept)
+        {
+            w = w_solved.head(w_solved.size() - 1);
+            b = w_solved[w_solved.size() - 1];
+        }
+        else
+        {
+            w = w_solved;
+            b = 0.0;
+        }
 
-    fit_flag = true;
-    std::cout << "Training complete. Coefficients: " << w.transpose() << ", bias: " << b << std::endl;
+        fit_flag = true;
+        std::cout << "Training complete. Coefficients: " << w.transpose() << ", bias: " << b << std::endl;
+        }
+    
+        else if (method == "gradient_descent")
+        {
+            const auto n_samples = X.rows();
+            const auto n_features = X.cols();
+            w = Eigen::VectorXd::Zero(X.cols());
+            b = 0.0;
+
+            for (int i = 0; i < epochs; ++i)
+            {
+                Eigen::VectorXd y_pred = X * w;
+                if (fit_intercept)
+                {
+                    y_pred.array() += b;
+                }
+
+                Eigen::VectorXd error = y_pred - y;
+                Eigen::VectorXd grad_w = (X.transpose() * error)/n_samples;
+
+                if (l2 > 0.0)
+                {
+                    grad_w += (l2/n_samples) * w;
+                }
+
+                double grad_b = 0.0;
+                if (fit_intercept)
+                {
+                    grad_b = error.mean();
+                }
+
+                w -= learning_rate * grad_w;
+                if (fit_intercept)
+                {
+                    b -= learning_rate * grad_b;
+                }
+
+                if (i%100 == 0)
+                {
+                    double loss = (error.squaredNorm()/(2 * n_samples));
+                    std::cout << "Epoch: " << i << " Loss: " << loss << std::endl;
+                }
+            }
+
+            fit_flag = true;
+            std::cout << "Training complete. Coefficients: " << w.transpose() << ", bias: " << b << std::endl;
+        }
+
+        else
+        {
+            throw std::invalid_argument("Unsupported training method: " + method);
+        }
+
+    
 }
 
 // Defining the predict method algorithm.
